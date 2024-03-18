3f79367f1efb9c33e8ba59d8f3fcc7f5642ef743
Ellen Christina <ellencas172@gmail.com>
2024-03-18 21:39:29 +0000

    design concluido; inserindo json

---
+++ js/news.js
@@ -0,0 +1,57 @@
+window.onload=function(){
+
+var data = 
+{
+    news:[
+        {
+            "type": "mundo",
+            "title": "Direita vence eleições em Portugal e, agora, vive um dilema",
+            "text": "<p>A polarização na política chegou em Portugal. Depois de 8 anos sendo governados pelo Partido Socialista, os portugueses decidiram virar à direita nas eleições do último final de semana.</p><p>A coligação de centro-direita conseguiu 79 cadeiras na Assembleia contra 77 dos socialistas. Mas mesmo com a vitória, o resultado não é suficiente para formar o governo.</p><blockquote>Para eleger um novo primeiro-ministro, parlamentares precisam fazer alianças entre partidos para somar, ao todo, pelo menos 115 das 230 cadeiras do total.</blockquote><p>E é aí que mora o impasse… Antes das eleições, a centro-direita garantiu que não iria se aliar ao partido da chamada ultradireita.</p><p>Acontece que o partido mais à direita passou de 1 deputado em 2019 para 48 representantes na última votação.</p><p>Com isso, se a direita como um todo não se unir, restam duas opções:</p><ul><li>Governar com minoria e correr o risco de ter um governo frágil que pode cair a qualquer momento;</li><li>Se juntar com a esquerda para isolar a ultradireita — situação mais improvável.</li></ul><p>Resumindo, ora pois: A direita ganhou as eleições em Portugal, mas ainda não tem como governar o país, e por isso os próximos dias vão ser cruciais para as negociações e alianças na Assembleia.</p>"
+        }, 
+        {
+            "type": "brasil",
+            "title": "Europa aumenta número de brasileiros com cidadania",
+            "text": "<p>Brasileiro sim. Europeu também. A Europa nunca concedeu tantas cidadanias para nascidos no Brasil quanto agora, e o número vem crescendo ano a ano. </p><blockquote>Em 2022, quase 26 mil brasileiros viraram cidadãos europeus — um aumento de 26% na comparação ano a ano. Entre todos os países, o Brasil ficou em 7º no ranking com mais cidadanias. </blockquote><p>De braços abertos: A Europa bateu recorde no número de identidades concedidas a estrangeiros. Foram quase 1 milhão de cidadanias entre os 27 países da UE.</p><p>A Itália puxou a fila, com mais de 210 mil documentos autorizados. Na sequência aparecem Espanha, Alemanha e França.</p><h3>O caso brasileiro</h3> <p>Diferente de outras nacionalidades que buscam asilo e refúgio depois de guerras e crises humanitárias, os brasileiros vão atrás da dupla cidadania por questões familiares. </p><p>Justamente por causa da quantidade de descendentes de italianos e portugueses — cerca de 40 milhões —, os dois países foram os que mais receberam pedidos vindos do Brasil. </p>    <blockquote>Em Portugal, os BRs lideraram a lista com mais de 6 mil concessões. Já na Itália, o governo concedeu 11 mil cidadanias a brasileiros — mais do que o dobro do ano anterior.</blockquote><p>Vieni con calma, Brasil! A onda de pedidos de cidadania tem irritado algumas cidades italianas, e o Parlamento do país está analisando um projeto de lei para limitar a cidadania só para quem fala italiano fluente. </p>"
+        }, 
+        {
+            "type": "tecnologia",
+            "title": "O TikTok pode ser banido para sempre nos EUA",
+            "text": "<p>Chega de dancinhas na Terra do Tio Sam? Está marcada para hoje à tarde na Câmara dos EUA a votação de um projeto de lei que quer banir o TikTok do país. </p><p>Se o texto for aprovado pelos parlamentares, o tico-teco vai ser excluído de todas as lojas de aplicativo do país. Quem baixou baixou, quem não baixou não baixa mais.</p><blockquote>A não ser que… O possível banimento seria suspenso caso a ByteDance — empresa dona do TikTok — vendesse o app para uma outra companhia que não seja da China.</blockquote><h4>O problema é o país asiático:<h4><ul><li> A inteligência americana diz que a China influencia as eleições dos EUA pela rede social; </li><li> Os parlamentares acreditam que o governo chinês espiona usuários e controla o algoritmo da plataforma. </li></ul><p>App bloqueado. 39 estados americanos já proibiram o TikTok em celulares corporativos de funcionários públicos. </p><h3>Apoio dos dois lados</h3><p>Embora o projeto tenha sido escrito por um republicano, os democratas também apoiam a proposta. O banimento deve ser aprovado hoje na Câmara. No Senado, a situação é mais incerta. </p><h4>E os candidatos?</h4><ul><li> Biden disse que vai assinar a lei caso ela passe no Congresso; </li><li> Trump chamou o TikTok de ameaça à segurança nacional, mas falou que o banimento faria a Meta crescer — empresa que ele chama de inimiga do povo. </li></ul><p>Zoom out: O EUA é o país onde o TikTok é mais popular, com quase 150 milhões de usuários. Não à toa, deputados e senadores estão recebendo milhares de ligações nos seus gabinetes pedindo para votarem contra o projeto. </p>"
+        }, 
+        {
+            "type": "negocios",
+            "title": "Wall Street pode ter dobradinha TECH de IPOs",
+            "text": "<p>O fundador do Telegram anunciou que a empresa pode estar a caminho de abrir seu capital em bolsa. </p><p>A empresa superou a marca de 900 milhões de usuários e espera atingir a tão esperada lucratividade no próximo ano, à medida que a receita de ads e de assinaturas tem subido. </p><blockquote>Agora, valendo US$ 30 bilhões, a companhia deve buscar o IPO — mas com a premissa de que vai “manter a independência”.</blockquote><h3>Enquanto isso…</h3><p>O Reddit, rede social conhecida por seus fóruns e memes virais, já confirmou que espera alcançar um valuation de aproximadamente US$ 6,5 bilhões em seu IPO. </p><p>Além disso, o Reddit vai separar parte das ações que vai listar para certos usuários e moderadores de suas páginas. Usuários —> Sócios.</p><p>Curiosidade: O IPO do Reddit será o primeiro em anos para uma rede social destaque — a última grande estreia na bolsa de valores dos EUA foi em 2019, com o Pinterest. </p>"
+        },
+        {
+            "type": "economia",
+            "title": "De olho nos juros, Brasil e EUA divulgam inflação de fevereiro",
+            "text": "<p>Lá e cá. No mesmo dia, foram divulgados os números da inflação brasileira e da americana no mês de fevereiro, fazendo os mercados reagirem aos avanços nos dois países. </p><h3>Do lado de cá</h3><p> A volta às aulas teve seu peso na inflação do Brasil, puxando a alta de 0,83% nos preços em fevereiro. </p><p>     Nos últimos 12 meses, o índice é de 4,5% — dentro da meta do Banco Central. </p><p> Os alimentos também pesaram no resultado, e analistas projetam que a onda de calor também afete a produção. </p><h3>Do lado de lá</h3><p> O índice de preços americano subiu 0,4% no mês passado, puxado principalmente pelo valor dos combustíveis. </p><p> Quando analisados nos últimos 12 meses, os resultados mostram um avanço de 3,2% na inflação — essa veio acima do esperado pelos economistas. </p><h3>O famoso fechamento</h3><p> Com a inflação mensal dentro da expectativa de Wall Street, as bolsas gringas fecharam aliviadas com amplas altas. Por aqui, a Bolsa BR acompanhou. </p><ul><li> 🇧🇷 Ibovespa: +1,22% | 127.668 pontos</li><li>🇺🇸 S&P 500: +1,12% | 5.175 pontos</li><li>💵 Dólar: R$ 4,97 (-0,07%)</li><li>💶 Euro: R$ 5,43 (-0,07%) </li></ul>"
+        }
+    ]
+};
+
+
+function changeMenuClass(atualClass){
+    
+    if(atualClass == "white") return "pink";
+    if(atualClass == "pink") return "purple";
+    if(atualClass == "purple") return "white";
+}
+
+menuClass = "white";
+data.news.forEach(
+    function(item){
+
+        var newMenu = "<li class='" + menuClass + "'><a href='#'>" 
+                    + "<p class='title'>" + item.title + "</p><p class='subtitle'>" 
+                    + item.type + "</p>";
+
+        document.querySelector('#sidenav').innerHTML += newMenu;
+
+        menuClass = changeMenuClass(menuClass);
+    }
+);
+
+
+}
